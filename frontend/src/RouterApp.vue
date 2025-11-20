1912fb01e77b070dab8050df9f3f1f0226cea99d
EXTR3MUS <diego7042@gmail.com>
2025-11-20 20:55:46 -0300

    feat: Add RouterApp component and configure routing in main.js

---
+++ frontend/src/RouterApp.vue
@@ -0,0 +1,12 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+export default {
+  name: 'RouterApp',
+  mounted() {
+    console.log('RouterApp mounted, current route:', this.$route.path)
+  }
+}
+</script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import RouterApp from './RouterApp.vue'
 import App from './App.vue'
 import AuditLogs from './views/AuditLogs.vue'
 import Unauthorized from './views/Unauthorized.vue'
+
+console.log('Main.js loading...')
 
 const routes = [
   { path: '/', name: 'home', component: App },
@@ -15,6 +18,13 @@ const router = createRouter({
   routes
 })
 
-const app = createApp(App)
+router.beforeEach((to, from, next) => {
+  console.log('Navigating from', from.path, 'to', to.path)
+  next()
+})
+
+const app = createApp(RouterApp)
 app.use(router)
 app.mount('#app')
+
+console.log('App mounted with router')
